76e5783202a72bfa0312f2e01a20d3a7f31ce023
asperlas <lasse.asperheim@tietoevry.com>
2023-03-21 22:06:48 +0100

    made helloWorld function take a parameter

--- helloWorld.py
+++ helloWorld.py
@@ -2,8 +2,8 @@
 # "But what does it do?", I hear you wonder.
 # Prepare to be amazed, then: it prints "Hello World!"
 
-def helloWorld():
-    print('Hello World!')
+def helloWorld(message : str):
+    print(message)
 
 # Call our wonderful function
 
