2136de60b6dc84d47e917b9b743a3af889320ea3
asperlas <lasse.asperheim@tietoevry.com>
2023-03-21 20:34:20 +0100

    Hello World!

---
+++ helloWorld.py
@@ -0,0 +1,10 @@
+# Define the exiting, cryptic function.
+# "But what does it do?", I hear you wonder.
+# Prepare to be amazed, then: it prints "Hello World!"
+
+def helloWorld():
+    print('Hello World!')
+
+# Call our wonderful function
+
+helloWorld()
